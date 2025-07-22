7c3d0ed9ab1f122f160cabe45df605d541734aae
Sonudhukia143 <jagdishdhukia770@gmail.com>
2025-07-22 15:19:22 +0530

    01 SetUp and Database Configuration Completed Initial Commit

---
+++ app/api/users/route.ts
@@ -0,0 +1,9 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// export default async function GET() {
+//     console.log('Fetching users from the database...');
+//     const users = await prisma.user.findMany();
+//     return Response.json(users);
+// }
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+  return (
+      <div>
+        NEXT APP
+      </div>
+  );
+}
